2e56108e04f95afcc7cd91800b981f63dfaf0326
wwwlaithraad1996102-oss <www.laithraad1996102@gmail.com>
2026-08-24 16:22:40 +0300

    config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // ضع Project URL و Publishable/Anon Key الخاصين بمشروعك هنا.
 // لا تضع service_role key في هذا الملف أو في المتصفح.
 window.SOKA_CONFIG = {
-  supabaseUrl: 'https://kvynoxkmyhjccppriphl.supabase.co/rest/v1/',
+  supabaseUrl: 'https://kvynoxkmyhjccppriphl.supabase.co',
   supabaseAnonKey: 'sb_publishable_2Bbv-K7fPvDFFlfU5e-Bvg_zYLL4LfS'
 };
