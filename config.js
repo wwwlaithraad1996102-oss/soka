b81ebddc356e4903778255fe241f2187746ebd7b
wwwlaithraad1996102-oss <www.laithraad1996102@gmail.com>
2026-08-25 05:03:33 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SOKA_CONFIG = {
   supabaseUrl: 'https://kvynoxkmyhjccppriphl.supabase.co',
-  supabaseAnonKey: ا'sb_publishable_2Bbv-K7fPvDFFlfU5e-Bvg_zYLL4LfS'
+  supabaseAnonKey: 'sb_publishable_2Bbv-K7fPvDFFlfU5e-Bvg_zYLL4LfS'
 };
